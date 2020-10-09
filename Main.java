9dbc53f167ec8517bf57756cc958aaa1159ddec8
qurek9038 <71150179+qurek9038@users.noreply.github.com>
2020-10-09 15:34:30 -0400

    Update Main.java
    Question A2Q2 Completed

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program determines which of the two integers the user inputs is the largest
+ * @author Khazina
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create the scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask the user for the first integer
+    System.out.println("Please enter an integer:");
+
+    //Store user input using a varible
+    int first = input.nextInt();
+
+    //Ask the user for the second integer
+    System.out.println("Please enter another integer:");
+
+    //Store user input using a second varible 
+    int second = input.nextInt(); 
+
+    //Responses that will display based on which number is bigger or if they are equal
+    if (first > second){
+      System.out.println("The biggest number is " + first + ".");
+    }
+    if (second > first){
+      System.out.println("The biggest number is " + second + ".");
+    }
+    if (first == second){
+      System.out.println("There is no biggest number.");
+    }
     
   }
 }
